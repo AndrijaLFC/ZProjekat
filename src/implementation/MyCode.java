7e5a6765c64b69fc007fddca9a6c1ce7964da680
AndrijaLFC <andrija.boricic@hotmail.com>
2018-06-01 17:46:59 +0200

    Working on loadKeyPair function

--- src/implementation/MyCode.java
+++ src/implementation/MyCode.java
@@ -1,13 +1,19 @@
 package implementation;
 
 import code.GuiException;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import x509.v3.CodeV3;
 
+import javax.naming.InvalidNameException;
+import javax.naming.ldap.LdapName;
 import java.io.*;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Collections;
 import java.util.Enumeration;
 
 public class MyCode extends CodeV3 {
@@ -18,6 +24,20 @@ public class MyCode extends CodeV3 {
     private static final String KEYSTORE_NAME = "ba140645d.p12";
 
     private static final String KEYSTORE_PASSWORD = "root";
+
+
+    private static final int KEYPAIR_ERROR_CODE = -1;
+
+    private static final int KEYPAIR_UNSIGNED_CODE = 0;
+
+    private static final int KEYPAIR_SIGNED_CODE = 1;
+
+    private static final int KEYPAIR_TRUSTED_CERT = 2;
+
+
+    private static final int VERSION_3_CODE = 2;
+
+    private static final int VERSION_1_CODE = 1;
 
 
     private KeyStore keyStore;
@@ -37,6 +57,7 @@ public class MyCode extends CodeV3 {
         try {
             // pozovemo metodu store
             keyStore.store(fos = new FileOutputStream(KEYSTORE_NAME), KEYSTORE_PASSWORD.toCharArray());
+
         } catch (KeyStoreException | IOException | CertificateException | NoSuchAlgorithmException e) {
             e.printStackTrace();
         }finally {
@@ -48,6 +69,47 @@ public class MyCode extends CodeV3 {
                 }
         }
     }
+
+    private void loadBasicCertificateInfo(X509Certificate certificate, X509Certificate issuer){
+
+        access.setPublicKeyAlgorithm(certificate.getSigAlgName());
+
+        access.setSubjectSignatureAlgorithm(certificate.getSigAlgName());
+
+        access.setSerialNumber(certificate.getSerialNumber().toString());
+
+        access.setNotBefore(certificate.getNotBefore());
+
+        access.setNotAfter(certificate.getNotAfter());
+
+        access.setVersion(certificate.getVersion() == 3? VERSION_3_CODE : VERSION_1_CODE);
+
+        for(String info : Collections.list())
+        try {
+            LdapName ldapName = new LdapName(certificate.getSubjectDN().toString());
+
+            String infoType = info.split("=")[0];
+
+            String infoValue = info.split("=")[1];
+
+            switch(infoType){
+                case "CN":  access.setSubjectCommonName(infoValue);         break;
+                case "C":   access.setSubjectCountry(infoValue);            break;
+                case "L":   access.setSubjectLocality(infoValue);           break;
+                case "O":   access.setSubjectOrganization(infoValue);       break;
+                case "OU":  access.setSubjectOrganizationUnit(infoValue);   break;
+                case "ST":  access.setSubjectState(infoValue);              break;
+            }
+            
+        } catch (InvalidNameException e) {
+            e.printStackTrace();
+        }
+
+
+        //access.setPublicKeyParameter();
+
+    }
+
     @Override
     public Enumeration<String> loadLocalKeystore() {
 
@@ -56,7 +118,7 @@ public class MyCode extends CodeV3 {
 
         try {
             // dohvatimo keystore
-            keyStore =  KeyStore.getInstance(KEYSTORE_TYPE);
+            keyStore =  KeyStore.getInstance(KEYSTORE_TYPE, new BouncyCastleProvider());
 
             // fajl sa svim kljucevima
             File keyStoreFile = new File(KEYSTORE_NAME);
@@ -90,7 +152,7 @@ public class MyCode extends CodeV3 {
         try {
 
             // moramo da instanciramo novi keystore
-            keyStore = KeyStore.getInstance(KEYSTORE_TYPE);
+            keyStore = KeyStore.getInstance(KEYSTORE_TYPE, new BouncyCastleProvider());
 
             // postavimo da je sadrzaj prazan
             keyStore.load(null, null);
@@ -104,7 +166,31 @@ public class MyCode extends CodeV3 {
     }
 
     @Override
-    public int loadKeypair(String s) {
+    public int loadKeypair(String keyPairName) {
+
+        X509Certificate issuerCertificate = null;
+
+        X509Certificate certificate = null;
+
+        Certificate[] chainedCertificates = null;
+
+
+        try {
+            certificate = (X509Certificate) keyStore.getCertificate(keyPairName);
+
+            chainedCertificates = keyStore.getCertificateChain(keyPairName);
+
+            if (chainedCertificates != null)
+                issuerCertificate = (X509Certificate) chainedCertificates[0];
+
+            loadBasicCertificateInfo(certificate, issuerCertificate);
+
+
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+
+
         return 0;
     }
 
